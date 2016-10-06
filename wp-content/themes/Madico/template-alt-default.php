a1db94a16db56d41fb4aa41f7712a0af9fe7c41e
eddt <etwilbeck@marketingincolor.com>
2016-10-06 15:26:15 -0500

    new distributor form part 1

---
+++ wp-content/themes/Madico/template-alt-default.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Template Name: Alternate Default
+ *
+ * @package     : WordPress
+ * @subpackage  : Twenty_Fifteen
+ * @Description : The alternate default page template for displaying content
+ * @Created At  : 6 October 2016
+ * @Modified At :
+ * @Created By  : eddt
+ * @Modified By :
+ */
+
+get_header('madico');
+?>
+<!-- content starts here -->
+<div class="contact-us-container">
+    <div class="container">
+        <?php
+		// Start the loop.
+		while ( have_posts() ) : the_post();
+
+			// Include the page content template.
+			get_template_part( 'content', 'page' );
+
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+
+		// End the loop.
+		endwhile;
+		?>
+    </div>
+</div>
+<!-- content starts here -->
+<?php
+get_footer('madico');
+?>
